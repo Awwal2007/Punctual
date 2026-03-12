439228b9aab68f4a5bc052687b99b62978529c5e
village-coders <villagecoders7@gmail.com>
2026-03-12 04:03:29 +0100

    sfsdfsd

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
